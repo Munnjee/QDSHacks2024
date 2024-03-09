b7b74735c486f720a219e66fd91adfce88a55f6c
Yongeun Kwon <ykwon35@my.bcit.ca>
2024-03-09 13:44:18 -0800

    Modified query

--- database/query.sql
+++ database/query.sql
@@ -13,13 +13,6 @@
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
   
-ALTER TABLE `freedb_QDSHacks2024`.`user` 
-ADD COLUMN `user_name` VARCHAR(45) NOT NULL AFTER `user_id`,
-CHANGE COLUMN `password` `password` VARCHAR(200) NOT NULL AFTER `last_name`,
-ADD UNIQUE INDEX `user_name_UNIQUE` (`user_name` ASC) VISIBLE;
-;
-
-  
   CREATE TABLE `freedb_QDSHacks2024`.`user_school` (
   `user_school_id` INT NOT NULL AUTO_INCREMENT,
   `frn_user_id` INT NOT NULL,
@@ -40,19 +33,18 @@ ADD UNIQUE INDEX `user_name_UNIQUE` (`user_name` ASC) VISIBLE;
 
   CREATE TABLE `freedb_QDSHacks2024`.`coverage` (
   `coverage_id` INT NOT NULL AUTO_INCREMENT,
-  `frn_insurance_id` INT NOT,
+  `frn_insurance_id` INT NOT NULL,
   `frn_category_id` INT NOT NULL,
   `percentage` INT NOT NULL,
   `limit` INT NOT NULL,
   PRIMARY KEY (`coverage_id`));
   
-  CREATE TABLE `freedb_QDSHacks2024`.`coverage_balance` (
-  `coverage_balance_id` INT NOT NULL AUTO_INCREMENT,
+  CREATE TABLE `freedb_QDSHacks2024`.`claim` (
+  `claim_id` INT NOT NULL AUTO_INCREMENT,
   `frn_user_school_id` INT NOT NULL,
   `frn_coverage_id` INT NOT NULL,
-  `balance` INT NOT NULL,
-  PRIMARY KEY (`coverage_balance_id`));
-
+  `amount` INT NOT NULL,
+  PRIMARY KEY (`claim_id`));
 
 
 CREATE TABLE `freedb_QDSHacks2024`.`category` (
@@ -61,6 +53,47 @@ CREATE TABLE `freedb_QDSHacks2024`.`category` (
   PRIMARY KEY (`category_id`));
 
 -- Foreign keys
+ALTER TABLE `freedb_QDSHacks2024`.`coverage` 
+ADD INDEX `coverage_frn_category_id_idx` (`frn_category_id` ASC) VISIBLE,
+ADD INDEX `coverage_frn_insurance_id_idx` (`frn_insurance_id` ASC) VISIBLE;
+;
+ALTER TABLE `freedb_QDSHacks2024`.`coverage` 
+ADD CONSTRAINT `coverage_frn_category_id`
+  FOREIGN KEY (`frn_category_id`)
+  REFERENCES `freedb_QDSHacks2024`.`category` (`category_id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION,
+ADD CONSTRAINT `coverage_frn_insurance_id`
+  FOREIGN KEY (`frn_insurance_id`)
+  REFERENCES `freedb_QDSHacks2024`.`insurance` (`insurance_id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ALTER TABLE `freedb_QDSHacks2024`.`coverage` 
 ADD INDEX `coverage_frn_category_id_idx` (`frn_category_id` ASC) VISIBLE,
 ADD INDEX `coverage_frn_school_id_idx` (`frn_school_id` ASC) VISIBLE;
@@ -125,11 +158,19 @@ JOIN coverage ON school.school_id = coverage.frn_school_id
 JOIN category ON coverage.frn_category_id = category.category_id
 WHERE school.school_name = 'BCIT';
 
--- User's remaining balance per category
-SELECT user.user_name, 
-FROM usercoverage
 
-
+-- school
+1	BCIT	Canada Life
+2	UBC	Pacific Blue Cross
+3	Langara	Pacific Blue Cross
+4	SFU	Pacific Blue Cross
+	
+-- category
+1	Prescription Drug
+2	Vision
+3	Dental
+4	Travel
+	
 
   
   
